e92b280f9d26552d48171981ff45fecbcd2e3b4b
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2024-11-28 09:49:30 +0100

    ci: trigger actions from command push
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- hack/comment.sh
+++ hack/comment.sh
@@ -3,11 +3,14 @@
 set -eu
 
 COMMENT="${1}"
-GITHUB_ENV="${2}"
+PR="${2}"
+GITHUB_ENV="${3}"
 
 if [[ "$COMMENT" =~ ^/ci[[:space:]]+([a-zA-Z0-9_-]+)[[:space:]]+([0-9]+\.[0-9]+\.[0-9]+)$ ]]; then
     echo "chart_name=${BASH_REMATCH[1]}" >> $GITHUB_ENV
     echo "chart_version=${BASH_REMATCH[2]}" >> $GITHUB_ENV
+    branch=$(gh pr view "${PR}" --json='headRefName' -q='.headRefName')
+    echo "branch=${branch}" >> $GITHUB_ENV
 else
     echo "Comment format invalid. Expected '/ci <chart-name> <chart-version>'." >&2
     exit 1
