3b6c7bc78466ecc8f51358e5939ea4da9bea270b
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2025-05-19 09:40:46 +0200

    ci: use autogen action
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- hack/autogen.sh
+++ hack/autogen.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-COMMENT="${1}"
+PR_TITLE="${1}"
 PR="${2}"
 GITHUB_ENV="${3}"
 SEMVER="${4:-semver}"
@@ -14,10 +14,15 @@ PW='[0-9]*[a-zA-Z-][0-9a-zA-Z-]*'
 # Regex for a semver build-metadata word
 MW='[0-9a-zA-Z-]+'
 
-if [[ "$COMMENT" =~ ^/ci[[:space:]]+([a-zA-Z0-9_-]+)[[:space:]]+(.+)$ ]]; then
-    echo "chart_name=${BASH_REMATCH[1]}" >> "${GITHUB_ENV}"
-    "${SEMVER}" validate "${BASH_REMATCH[2]}"
-    echo "chart_version=${BASH_REMATCH[2]}" >> "${GITHUB_ENV}"
+# Match: chore(main): release <chart-name> <chart-version>
+if [[ "$PR_TITLE" =~ ^chore\(main\):[[:space:]]+release[[:space:]]+([a-zA-Z0-9_-]+)[[:space:]]+(.+)$ ]]; then
+    chart_name="${BASH_REMATCH[1]}"
+    chart_version="${BASH_REMATCH[2]}"
+
+    echo "chart_name=${chart_name}" >> "${GITHUB_ENV}"
+    "${SEMVER}" validate "${chart_version}"
+    echo "chart_version=${chart_version}" >> "${GITHUB_ENV}"
+
     branch=$(gh pr view "${PR}" --json='headRefName' -q='.headRefName')
     echo "branch=${branch}" >> "${GITHUB_ENV}"
 
@@ -27,6 +32,6 @@ if [[ "$COMMENT" =~ ^/ci[[:space:]]+([a-zA-Z0-9_-]+)[[:space:]]+(.+)$ ]]; then
         exit 1
     fi
 else
-    echo "Comment format invalid. Expected '/ci <chart-name> <chart-version>'." >&2
+    echo "PR title format invalid. Expected: 'chore(main): release <chart-name> <chart-version>'." >&2
     exit 1
 fi
